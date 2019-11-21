e5b295a971ac41cb3ab8cfe85d04fbcbcb4a274d
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-11-21 17:01:21 -0500

    adding files for initial setup

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+// users - recipes (many to many)
+// through table called saved_recipes
+
+// users - fridge (one to one)
+
+// ingredients - fridge (many to many)
+// through table called fridge_stock
