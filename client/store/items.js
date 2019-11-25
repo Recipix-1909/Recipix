616dd5cfa45e943854556ad7396e55e99b105692
Chris Chin <chin.christopher@gmail.com>
2019-11-25 14:29:46 -0500

    worked on thunk for saving item

--- client/store/items.js
+++ client/store/items.js
@@ -1,44 +1,55 @@
-import axios from 'axios'
-
+import axios from "axios";
+import { edamamFoodAPIID, edamamFoodAPIKEY } from "../../secrets";
 
 //action type
 
-const SAVE_ITEM = 'SAVE_ITEM'
-const GET_ITEM = 'GET_ITEM'
-
+const SAVE_ITEM = "SAVE_ITEM";
+const GET_ITEM = "GET_ITEM";
 
 //action creator
 
 const getItem = item => {
-    return{
-        type: GET_ITEM,
-        item
-    }
-}
+  return {
+    type: GET_ITEM,
+    item
+  };
+};
 
 const saveItem = item => {
-    return{
-        type: SAVE_ITEM,
-        item
-    }
-}
+  return {
+    type: SAVE_ITEM,
+    item
+  };
+};
 
 //thunk
 export const saveItemThunk = (userId, item) => {
-    return async dispatch => {
-        const{data} = await axios.get('/api/item/')
-        dispatch(getItem(data))
-        if(data.serialNum){
-            await axios.post(`/api/fridge/${userId}/add`, item)
-        }else{
-
-            
-            await axios.post(`/api/fridge/${userId}/add`, item)
-            await axios.post('/api/item/', item)
-        }
-        dispatch(saveItem(data))
+  return async dispatch => {
+    const { data } = await axios.get("/api/item/");
+    dispatch(getItem(data));
+    if (data.serialNum) {
+      await axios.post(`/api/fridge/${userId}/add`, item);
+    } else {
+      // API call to Edamam
+      const { data } = await axios.get(
+        `https://api.edamam.com/api/food-database/parser?upc=${data.serialNum}&app_id=${edamamFoodAPIID}&app_key=${edamamFoodAPIKEY}`
+      );
+      const edamamItem = {
+        name: data.hints.food.label,
+        serialNum: data.text.slice(4),
+        imageUrl: data.hints.food.image
+      };
+      await axios.post(`/api/fridge/${userId}/add`, edamamItem);
+      await axios.post("/api/item/", edamamItem);
     }
-}
+    dispatch(saveItem(data));
+  };
+};
 
-
-const itemsReducer = (items={}, action)
+const itemsReducer = (items = {}, action) => {
+  switch (action.type) {
+    case SAVE_ITEM: {
+      return { ...items, item: action.item };
+    }
+  }
+};
