e9ecd766d56300dd9e365d69ddaafcfa1f7343df
Chris Chin <50430524+chrischindev@users.noreply.github.com>
2019-11-26 18:33:18 -0500

    Update recipes.js

--- client/store/recipes.js
+++ client/store/recipes.js
@@ -9,7 +9,7 @@ const GET_RECIPES = "GET_RECIPES";
 //action creator
 
 const getRecipes = recipe => {
-<<<<<<< HEAD
+
     return{
         type: GET_RECIPES,
         recipe
@@ -28,27 +28,7 @@ export const getRecipesThunk = (userId) => {
 
     }
 }
-=======
-  return {
-    type: GET_RECIPES,
-    recipe
-  };
-};
 
-//thunk
-
-export const getRecipesThunk = userId => {
-  return async dispatch => {
-    console.log("WE ARE IN THE THUNK");
-    const { data } = await axios.get(
-      `http://172.16.21.172:8080/api/recipes/${userId}`
-    );
-    console.log("data at 0", data[0]);
-    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", data);
-    dispatch(getRecipes(data));
-  };
-};
->>>>>>> master
 
 //reducer
 
@@ -62,8 +42,5 @@ const recipesReducer = (recipes = {}, action) => {
   }
 };
 
-<<<<<<< HEAD
-export default recipesReducer
-=======
 export default recipesReducer;
->>>>>>> master
+
