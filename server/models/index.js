f5c8d2dda5466a8dbc693673dd2c3f1ec0011285
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-11-26 13:05:06 -0500

    removed server from front end

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-// users - recipes (many to many)
-// through table called saved_recipes
-
-// users - fridge (one to one)
-
-// ingredients - fridge (many to many)
-// through table called fridge_stock
