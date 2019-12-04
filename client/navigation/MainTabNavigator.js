a6e198b734dce588fe395e414c9e18c9d138599e
Luis Bello <luisbg9@gmail.com>
2019-12-04 16:14:31 -0500

    styling tabbar

--- client/navigation/MainTabNavigator.js
+++ client/navigation/MainTabNavigator.js
@@ -95,7 +95,7 @@ const tabNavigator = createBottomTabNavigator(
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused }) => {
         const { routeName } = navigation.state
-        let focusedColor = focused ? '#1d2e3b' : '#8fafc8'
+        let focusedColor = focused ? '#00ffcc' : '#8fafc8'
         if (routeName === 'CameraStack') {
           return (
             <Icon
